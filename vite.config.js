ccc0116b5dc0ad010482cb5d57173de58fb64376
Samira Dodson <entertainment.lab@rare.org>
2025-06-30 11:34:13 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TV-and-Entertainment/data/4-Outputs/heatwave-plot-react/',
+  base: '/greys-anatomy-report/', // updated base path
   build: {
     outDir: 'dist'
   }
